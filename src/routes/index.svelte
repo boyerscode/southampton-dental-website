1ebc95293f5754acda2becf7116da4c7a2b78f38
boyerscode <30.jboyer@gmail.com>
2022-02-22 17:05:57 -0600

    One More Try

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<nav>
+    <ul>
+      <li><a href="#intro">Home</a></li>
+      <li><a href="/about">About Us</a></li>
+      <li><a href="#portfolio">Gallery</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#pricing">Pricing</a></li>
+      <li><a href="#team">Team</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+  <style>
+      ul{
+          display: flex;
+          list-style: none;
+          gap: 1rem;
+      }
+  </style>
