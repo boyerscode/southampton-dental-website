e34fd19f9f2b1a451810cf395131cd879b4d117a
boyerscode <30.jboyer@gmail.com>
2022-03-08 07:09:52 -0600

    page building

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,110 @@
-<h1>About</h1>
-<p>This is the about page</p>
+<section id="about-us">
+    <div class="container">
+      <h2 class="section-title"><span>About Us</span></h2>
+      <p class="section-desc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+        veniam, debitis natus aperiam porro. Neque ratione voluptates tempore
+        unde ab ex veniam blanditiis quidem aliquam eveniet mollitia,
+        architecto facere omnis.
+      </p>
+      <div class="works-container">
+        <div class="work first">
+          <div class="work-icon">
+            <i class="far fa-edit"></i>
+          </div>
+          <div class="work-title">
+            <p>CONTENT FILLER</p>
+          </div>
+          <div class="work-desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo
+              optio, officia debitis facilis ducimus, cumque voluptatibus
+              perspiciatis, saepe qui a, culpa numquam est pariatur dicta
+              consequatur neque nemo fugit tempora!
+            </p>
+          </div>
+        </div>
+        <div class="work">
+          <div class="work-icon">
+            <i class="far fa-gem"></i>
+          </div>
+          <div class="work-title">
+            <p>OUR PROMISE TO YOU</p>
+          </div>
+          <div class="work-desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo
+              optio, officia debitis facilis ducimus, cumque voluptatibus
+              perspiciatis, saepe qui a, culpa numquam est pariatur dicta
+              consequatur neque nemo fugit tempora!
+            </p>
+          </div>
+        </div>
+        <div class="work">
+          <div class="work-icon">
+            <i class="far fa-lightbulb"></i>
+          </div>
+          <div class="work-title">
+            <p>WHAT DO WE DO?</p>
+          </div>
+          <div class="work-desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo
+              optio, officia debitis facilis ducimus, cumque voluptatibus
+              perspiciatis, saepe qui a, culpa numquam est pariatur dicta
+              consequatur neque nemo fugit tempora!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- about-us-end   -->
+
+  <style>
+      /*about us start*/
+#about-us{
+    background-color: #fff;
+}
+.section-title{
+    text-align: center;
+    text-transform: uppercase;
+    padding: 10px 0px;
+}
+.section-title span{
+    color: #5e7ce2;
+}
+.section-desc{
+    text-align: center;
+    font-style: italic;
+    padding: 5px 0px;
+    margin-bottom: 30px;
+}
+.work{
+    margin: 20px 0px;
+    text-align: center;
+}
+#about-us .work .work-icon{
+    font-size: 3em;
+    color: #5e7ce2;
+    padding: 10px 0px;
+}
+#about-us .work .work-title{
+    font-size: 1.3em;
+    font-weight: bold;
+    padding: 10px 0px;
+}
+.detail-text ul{
+    list-style-type: circle;
+    padding-left:20px;
+}
+.detail-text li{
+    font-style: italic;
+    padding: 5px;
+}
+.detail-img{
+    margin: 20px 0px;
+}
+/*about us end*/
+
+  </style>
