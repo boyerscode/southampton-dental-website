11b0fd18c6d24f0a37a67805a6776c0f89468e52
boyerscode <30.jboyer@gmail.com>
2022-03-22 08:03:21 -0500

    Updated Contact Page and Centered Nav bar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,21 @@
 <nav>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/about">About Us</a></li>
-      <li><a href="/services">Services</a></li>
-      <li><a href="/gallery">Gallery</a></li>
-      <li><a href="/pricing">Pricing</a></li>
-      <li><a href="/team">Team</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
-  </nav>
-  <slot></slot>
-  <style>
-      ul{
-          display: flex;
-          list-style: none;
-          gap: 1rem;
-      }
-  </style>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About Us</a></li>
+		<li><a href="/services">Services</a></li>
+		<li><a href="/gallery">Gallery</a></li>
+		<li><a href="/pricing">Pricing</a></li>
+		<li><a href="/team">Team</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
+<slot />
+
+<style>
+	ul {
+		display: flex;
+		list-style: none;
+		gap: 1rem;
+		justify-content: center;
+	}
+</style>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,2 +1,99 @@
-<h1>contact</h1>
-<p>This is the contact page</p>
+<body>
+	<!--  contact-start  -->
+	<section id="contact-us">
+		<div class="container">
+			<h2 class="section-title">Contact <span>Us</span></h2>
+			<p class="section-desc">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem odit architecto expedita,
+				eum possimus delectus nulla similique? Illum animi perferendis excepturi repellat,
+				voluptatum aliquid culpa unde praesentium, ipsam, odit repellendus.
+			</p>
+			<form action="">
+				<label for="name">*Name</label>
+				<input type="text" id="name" required />
+				<label for="email">*Email</label>
+				<input type="email" id="email" required />
+				<label for="subject">*Subject</label>
+				<input type="text" id="subject" required />
+				<label for="msg">*Message</label>
+				<textarea id="msg" required />
+				<button type="submit" class="btn-bg">Send message</button>
+			</form>
+		</div>
+	</section>
+	<!--  contact-end  -->
+
+	<!--  map-start -->
+	<section id="map">
+		<div class="mapouter">
+			<div class="gmap_canvas">
+				<iframe
+					id="gmap_canvas"
+					title="Southampton Dental Map"
+					width="100%"
+					height="500"
+					src="https://maps.google.com/maps?q=1501%20Hampton%20Ave%2C%20St.%20Louis%2C%20MO%2063139&t=&z=13&ie=UTF8&iwloc=&output=embed"
+					frameborder="0"
+					scrolling="no"
+					marginheight="0"
+					marginwidth="0"
+				/>
+			</div>
+			<style>
+				.mapouter {
+					overflow: hidden;
+					height: 500px;
+					width: 100%;
+				}
+				.gmap_canvas {
+					background: none !important;
+					height: 500px;
+					width: 100%;
+				}
+			</style>
+		</div>
+	</section>
+</body>
+
+<!--  map-end -->
+<style>
+	.container {
+		max-width: 1100px;
+		margin: auto;
+		padding: 10px;
+	}
+	form {
+		background-color: #fff;
+		padding: 10px;
+	}
+	form label {
+		padding: 5px 0px;
+		display: block;
+	}
+	form input,
+	form textarea {
+		width: 100%;
+		border: 2px solid transparent;
+		outline: none;
+		padding: 10px;
+		margin-bottom: 10px;
+		background-color: #f4f4f4;
+		transition: border 0.4s, background-color 0.4s;
+	}
+	form input:hover,
+	form textarea:hover,
+	form input:focus,
+	form textarea:focus {
+		border: 2px solid #f4f4f4;
+		background-color: #fff;
+	}
+	form textarea {
+		height: 80px;
+	}
+	form .btn-bg {
+		margin: 10px 0px;
+	}
+	#map {
+		padding-bottom: 0px;
+	}
+</style>
